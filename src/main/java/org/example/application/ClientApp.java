eba9a4334c0d6069ae956d011d90f75ff16017b9
aichatfamily <aichatfamily@gmail.com>
2025-06-28 15:49:47 +0800

    refactor: use logger

--- src/main/java/org/example/application/ClientApp.java
+++ src/main/java/org/example/application/ClientApp.java
@@ -3,8 +3,12 @@ package org.example.application;
 import io.temporal.client.WorkflowClient;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import org.example.greeting.GreetingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ClientApp {
+    
+    private static final Logger logger = LoggerFactory.getLogger(ClientApp.class);
     
     public static void main(String[] args) {
         WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
@@ -13,7 +17,7 @@ public class ClientApp {
         GreetingService greetingService = new GreetingService(client);
         String result = greetingService.sayHello("World");
         
-        System.out.println(result);
+        logger.info("Greeting result: {}", result);
         System.exit(0);
     }
 }
